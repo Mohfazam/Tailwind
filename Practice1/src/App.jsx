ac3d076935df6181b6bac9871a2bb493cbcbe374
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-05 00:42:52 +0530

    Practice1

--- Practice1/src/App.jsx
+++ Practice1/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className='sm:flex sm:justify-center'>
-        <div className='bg-green-300 text-orange-600'>Child 1</div>
+        <div className='bg-green-300 text-orange-600 text-lg rounded-bl-full rounded-br-full rounded-tr-full rounded-tl-full'>Child 1</div>
         <div className='bg-red-50 text-violet-900'>Child 2</div>
         <div className='bg-yellow-300 text-pink-600'>Child 3</div>
       </div>
