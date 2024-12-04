338d22968310c6dbb3ebf96fc71fdd938b604033
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-04 21:12:45 +0530

    Bug Fixes

--- Practice1/src/App.jsx
+++ Practice1/src/App.jsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <>
       <div className='sm:flex sm:justify-center'>
-        <div className='bg-green-300'>Child 1</div>
-        <div className='bg-red-300'>Child 2</div>
-        <div className='bg-yellow-300'>Child 3</div>
+        <div className='bg-green-300 text-orange-600'>Child 1</div>
+        <div className='bg-red-50 text-violet-900'>Child 2</div>
+        <div className='bg-yellow-300 text-pink-600'>Child 3</div>
       </div>
     </>
   )
