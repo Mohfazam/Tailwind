69bee46514da6f06a6ec379d4c71e14a1b7bc5a0
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-06 01:52:35 +0530

    Timer

--- otp/src/App.jsx
+++ otp/src/App.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Otp } from "./components/otp";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(60);
+
+  useEffect(() => {
+   if(count > 0){
+    const timer = setInterval(() => {
+      setCount((prevCount) => {
+        if(prevCount <= 1){
+          clearInterval(timer);
+        }
+        return prevCount - 1;
+      });
+    }, 1000);
+    return () => clearInterval(timer);
+   }
+  }, [])
 
   return (
     <div className="flex justify-center items-center bg-blue-800 w-screen h-screen">
@@ -14,6 +28,11 @@ function App() {
       <div className="absolute top-[200px]">
         <h1 className="text-white text-4xl">Check your Email for the code</h1>
       </div>
+      <Otp />
+      <br />
+      <div>
+        {count > 0 ? `Time remaining: ${count}` : <h1 className="text-white text-lg">Time is Up</h1>}
+      </div>
     </div>
     
   );
@@ -22,25 +41,3 @@ function App() {
 export default App;
 
 
-
-
-// import { useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div className="flex justify-center items-center bg-blue-800 w-screen h-screen">
-//       <div className="absolute top-[100px] text-white">
-//         <h1 className="text-4xl">Webinar.gg</h1>
-//       </div>
-
-//       <div className="absolute top-[200px] text-white">
-//         <h1 className="text-2xl">Check your Email for the code</h1>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
--- otp/src/components/otp.jsx
+++ otp/src/components/otp.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export function Otp() {
+    
   return (
-    <div>
+    <div className='flex justify-center items-center space-x-2'>
       <SubBoxes />
       <SubBoxes />
       <SubBoxes />
@@ -17,7 +18,26 @@ export function Otp() {
 function SubBoxes(){
     return(
         <div>
-            <input type="text" className='flex m-2 w-[40px] h-[50] rounded-2xl bg-blue-500'/>
+            <input 
+              type="text" 
+              maxLength={1}
+              className='
+                w-12 
+                h-12 
+                text-center 
+                text-white 
+                bg-blue-500 
+                border-2 
+                border-blue-600 
+                rounded-lg 
+                focus:border-blue-300 
+                focus:ring-2 
+                focus:ring-blue-200 
+                transition-all 
+                duration-300 
+                outline-none
+              '
+            />
         </div>
     )
 }
