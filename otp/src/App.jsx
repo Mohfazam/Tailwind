e663f61a26963af4b03fba7c7d4820ef63085010
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-09 00:45:44 +0530

    Auto Box Shifting Functionality

--- otp/src/App.jsx
+++ otp/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Otp } from "./components/otp";
 import "./App.css";
 
--- otp/src/components/otp.jsx
+++ otp/src/components/otp.jsx
@@ -1,24 +1,56 @@
+import { useState, useEffect, useRef } from "react";
 import React from 'react'
 
 export function Otp() {
+
+  const ref1 = useRef();
+  const ref2 = useRef();
+  const ref3 = useRef();
+  const ref4 = useRef();
+  const ref5 = useRef();
+  const ref6 = useRef();
+  const ref7 = useRef();
     
   return (
     <div className='flex justify-center items-center space-x-2'>
-      <SubBoxes />
-      <SubBoxes />
-      <SubBoxes />
-      <SubBoxes />
-      <SubBoxes />
-      <SubBoxes />
-      <SubBoxes />
+      <SubBoxes reference={ref1} onDone={()=> {
+        ref2.current.focus();
+      }} />
+
+      <SubBoxes reference={ref2} onDone={()=> {
+        ref3.current.focus();
+      }}/>
+
+      <SubBoxes reference={ref3} onDone={()=> {
+        ref4.current.focus();
+      }}/>
+
+      <SubBoxes reference={ref4} onDone={()=> {
+        ref5.current.focus();
+      }}/>
+
+      <SubBoxes reference={ref5} onDone={()=> {
+        ref6.current.focus();
+      }}/>
+
+      <SubBoxes reference={ref6} onDone={()=> {
+        ref7.current.focus();
+      }}/>
+
+      <SubBoxes reference={ref7} onDone={()=> {
+        ref7.current.focus();
+      }}/>
     </div>
   )
 }
 
-function SubBoxes(){
+function SubBoxes({reference, onDone}){
     return(
         <div>
-            <input 
+            <input ref={reference}
+            onChange={(e) => {
+              onDone();
+            }}
               type="text" 
               maxLength={1}
               className='
