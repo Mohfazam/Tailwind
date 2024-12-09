ea68c66cb5d33140dd4a2ec8e25292948a75c253
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-09 22:37:53 +0530

    Transition Effect

--- Profile/src/components/test1.jsx
+++ Profile/src/components/test1.jsx
@@ -12,19 +12,3 @@ export function Test1({children}){
     </div>
   )
 }
-
-
-
-
-// export function Test1({ children }){
-//   return (
-//     <div className="flex text-black">
-//       <div className="w-96 h-screen bg-blue-900 p-4 hidden md:block">
-//         sidebar
-//       </div>    
-//       <div className="w-61 bg-blue-200 p-4 w-screen h-screen" >
-//         {children}
-//       </div>
-//     </div>
-//   )
-// }
