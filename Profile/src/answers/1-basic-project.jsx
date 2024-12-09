836e068ee306822295b1563f2847a86b89a5d8e3
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-09 19:02:43 +0530

    Side bar and Content Styles

--- Profile/src/answers/1-basic-project.jsx
+++ Profile/src/answers/1-basic-project.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export function Basic_project(){
   return (
-    <div className="flex justify-between w-fit h-fit text-black">
-      <div className="w-61 bg-blue-900 p-4">
+    <div className="flex text-black">
+      <div className="w-96 h-screen bg-blue-900 p-4">
         sidebar
       </div>    
-      <div className="w-61 bg-blue-200 p-4" >
+      <div className="w-61 bg-blue-200 p-4 w-full" >
         content
       </div>
     </div>
