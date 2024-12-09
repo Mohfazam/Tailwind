0e66925bcb6f0d2024abd1086785183ac4f7c2ef
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-10 03:11:11 +0530

    SideBar animation

--- Profile/src/components/test1.jsx
+++ Profile/src/components/test1.jsx
@@ -1,14 +1,12 @@
-import React, { Children } from 'react'
+import React, { Children } from "react";
 
-export function Test1({children}){
+export function Test1({ children }) {
   return (
     <div className="flex text-black">
-      <div className="transition-all hidden delay-1000 h-screen bg-blue-900 p-4 md:block md:w-2/4">
+      <div className="transition-all duration-1000 h-screen bg-blue-900 p-4 md:block md:w-40 w-2">
         sidebar
-      </div>    
-      <div className="w-61 bg-blue-200 p-4 w-screen h-screen" >
-        {children}
       </div>
+      <div className="w-61 bg-blue-200 p-4 w-screen h-screen">{children}</div>
     </div>
-  )
+  );
 }
