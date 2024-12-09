73c324b33db2e53f8b62cb35f04e7f633ce1856a
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-09 18:40:49 +0530

    Initial Styling

--- Profile/src/answers/1-basic-project.jsx
+++ Profile/src/answers/1-basic-project.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export function Basic_project(){
   return (
-    <div className="flex justify-between w-full h-screen">
-      <div className="w-61 bg-grey-200 p-4">
+    <div className="flex justify-between w-fit h-fit text-black">
+      <div className="w-61 bg-blue-900 p-4">
         sidebar
       </div>    
-      <div className="w-61 bg-grey-200 p-4">
+      <div className="w-61 bg-blue-200 p-4" >
         content
       </div>
     </div>
