fdb5712e797944c507a8dda5d7c3e02f41a60d55
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-10 02:54:03 +0530

    Sidebar hiding Functionalit

--- Profile/src/components/test1.jsx
+++ Profile/src/components/test1.jsx
@@ -3,7 +3,7 @@ import React, { Children } from 'react'
 export function Test1({children}){
   return (
     <div className="flex text-black">
-      <div className="w-96 h-screen bg-blue-900 p-4 hidden md:block">
+      <div className="transition-all hidden delay-1000 h-screen bg-blue-900 p-4 md:block md:w-2/4">
         sidebar
       </div>    
       <div className="w-61 bg-blue-200 p-4 w-screen h-screen" >
