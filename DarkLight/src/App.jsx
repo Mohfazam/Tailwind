d3a63fe1a2a54a61e2f503f3193ea5ae318d756f
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-13 00:54:15 +0530

    Toggle Functionality

--- DarkLight/src/App.jsx
+++ DarkLight/src/App.jsx
@@ -4,31 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setdarkMode] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={`h-screen ${darkMode ? 'bg-white' : 'bg-black'} `}>
+      <button onClick={() => setdarkMode(!darkMode)} className={`${darkMode ? 'text-black' : 'text-white'}`}>Toggle</button>
+    </div>
   )
 }
 
