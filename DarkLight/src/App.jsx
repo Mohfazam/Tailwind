cc9cc442e4658cc2a3ec48f37c8ab379fb973b77
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-14 17:38:26 +0530

    Dark mode

--- DarkLight/src/App.jsx
+++ DarkLight/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [darkMode, setdarkMode] = useState(false);
 
   return (
-    <div className={`h-screen ${darkMode ? 'bg-white' : 'bg-black'} `}>
+    <div className={`h-screen bg-white dark:bg-black `}>
       <button onClick={() => setdarkMode(!darkMode)} className={`${darkMode ? 'text-black' : 'text-white'}`}>Toggle</button>
     </div>
   )
