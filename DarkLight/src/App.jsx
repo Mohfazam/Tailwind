78a63ecde77fa29c2646c0c77174883ba2f63820
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-26 01:58:20 +0530

    Initial Commit

--- DarkLight/src/App.jsx
+++ DarkLight/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [darkMode, setdarkMode] = useState(false);
 
   return (
-    <div className={`h-screen bg-white dark:bg-black `}>
-      <button onClick={() => setdarkMode(!darkMode)} className={`${darkMode ? 'text-black' : 'text-white'}`}>Toggle</button>
+    <div className={`h-screen ${darkMode ? 'bg-white' : 'bg-black'} `}>
+      <button onClick={() => setdarkMode(!darkMode)} className={`${darkMode ? 'text-black' : 'text-white'} `}>Toggle</button>
     </div>
   )
 }
